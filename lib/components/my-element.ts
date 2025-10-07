ffac14f046bf8a1d33b0e295903e3f26edde3838
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-10-07 10:29:42 +0200

    fix: tailwind borders

--- lib/components/my-element.ts
+++ lib/components/my-element.ts
@@ -16,6 +16,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        outlined:
+          "border hover:bg-gray-600/10",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
       },
